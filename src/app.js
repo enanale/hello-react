b9eef0e4045221eafa034a271dc0c520cadc26e2
Ed Nanale <enanale@gmail.com>
2019-11-09 17:09:26 -0800

    Inputs, two way binding, lifted state

--- src/app.js
+++ src/app.js
@@ -3,27 +3,64 @@ import React from 'react'
 import h from 'react-hyperscript'
 import './styles/app.scss'
 
-var things = ['A', 'B', 'C']
-
-function ThingListItem (props) {
- return h('li', {key: props.name}, `Item ${props.name}`)
+class ThingList extends React.Component {
+  render () {
+    return h('ul', this.props.things.map(i => (
+      h(ThingListItem, {name: i})
+    )))
+  }
 }
 
-function AddItem (props) {
-  return [
-    h('input', {type: 'text'}),
-    h('button', {type: 'button'}, 'Add Item' )
-  ]
+function ThingListItem (props) {
+  return h('li', {key: props.name}, `Item ${props.name}`)
+}
+
+class AddItem extends React.Component {
+  constructor (props) {
+    super(props)
+    this.newItem = null
+    this.addItem = props.addItem
+  }
+
+  onAddItem () {
+    this.addItem(this.newItem.value)
+    this.newItem.value = ''
+  }
+
+  render (props) {
+    return [
+      h('input', {
+        type: 'text',
+        ref: (e) => {this.newItem = e}
+      }),
+      h('button', {
+        type: 'button',
+        onClick: this.onAddItem.bind(this)
+      }, 'Add Item' )
+    ]
+  }
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      things: ['A', 'B', 'C']
+    }
+  }
+
+  addItem (s) {
+    // let newThings = this.state.things.slice()
+    // newThings.push(s)
+    this.state.things.push(s)
+    this.setState({things: this.state.things})
+  }
+
   render () {
     return [
-      h('h1', 'Hello, world'),
-      h('ul', things.map(i => (
-        h(ThingListItem, {name: i})
-      ))),
-      h(AddItem)
+      h('h1', `Hello, world ${this.state.things.length}`),
+      h(ThingList, {things: this.state.things}),
+      h(AddItem, {addItem: this.addItem.bind(this)})
     ]
   }
 }
